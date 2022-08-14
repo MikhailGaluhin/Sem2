563ea7ba2e988cce1b52c3f85d41413279880ea7
NensAleksa <108027067+NensAleksa@users.noreply.github.com>
2022-08-14 23:54:56 +0300

    решили задачу 10

---
+++ Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int showSecondValue(int number)
+{
+    int number3 = number % 10;
+    int number1 = number % 100;
+    int secondValue = number1 / 10;
+
+    return secondValue;
+}
+
+int secondValue = showSecondValue(number);
+Console.WriteLine(secondValue);
+
+showSecondValue(number);
