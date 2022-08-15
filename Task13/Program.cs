045e3a79edcfc34d5211f951af37320509b39280
NensAleksa <108027067+NensAleksa@users.noreply.github.com>
2022-08-16 01:16:46 +0300

    встрял на задаче 13

---
+++ Task13/Program.cs
@@ -0,0 +1,31 @@
+﻿
+//Огромная проблема с пониманием, как совмещать классы. Не понимаю как 
+//как вывести этот null, что б он не конфликтовал с int
+
+Console.WriteLine("Введите число");
+var? number = Convert.ToInt32(Console.ReadLine());
+
+var showThirdValue(int number)
+{
+    if (number % 10 > 0)
+    {
+        var? thirdValue = number % 10;
+        return thirdValue;
+    }
+    else
+    {
+        return null;
+    }
+}
+
+showThirdValue(number);
+
+var? thirdValue = showThirdValue(number);
+if (number == null)
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine ("number");
+}
